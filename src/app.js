02693a6f2ddcd318767e950951c48339ceece5b7
LexilesR <lexilesrivas@gmail.com>
2024-02-03 13:50:10 -0400

    maz kanvioz

--- src/app.js
+++ src/app.js
@@ -2,17 +2,21 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
+
 // motor de vistas
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+
 // archivos estaticos
-app.use(express.static(path.join(__dirname,'../public')))
+app.use(express.static(path.join(__dirname,'../public')));
 app.use((req,res)=>{
-  res.sendFile(path.join(__dirname,'../public/404.html'))
-})
+  res.sendFile(path.join(__dirname,'../public/404.html'));
+});
+app.use(express.urlencoded({extended: false}));
+
 
 //servidor en escucha
 app.listen(3000, () => {
     console.log('Servidor en espera')
-  })
+  });
