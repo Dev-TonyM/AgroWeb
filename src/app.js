0a6741733d9020f21bc1397c5feb855fb4c63842
Elranitacopter <elranacopter@gmail.com>
2024-02-02 21:00:17 -0400

    app making

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+// motor de vistas
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// archivos estaticos
+app.use(express.static(path.join(__dirname,'../public')))
+app.use((req,res)=>{
+  res.sendFile(path.join(__dirname,'../public/404.html'))
+})
+
+//servidor en escucha
+app.listen(3000, () => {
+    console.log('Servidor en espera')
+  })
---
+++ src/db.js
@@ -0,0 +1,7 @@
+const mysql = require("mysql");
+module.exports = mysql.createPool({
+  host: "sql10.freemysqlhosting.net:3306",
+  user: "sql10681146",
+  password: "IlBQ6t6zD5",
+  database: "sql10681146"
+});
